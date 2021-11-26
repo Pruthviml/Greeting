3414dd70218d90053c8624c23f221c6183cb32bd
Pruthviml <pruthvi.ml@signavio.com>
2021-11-26 13:13:49 +0100

    requirement1 is done

--- src/main/java/com/signavio/pair/Greetings.java
+++ src/main/java/com/signavio/pair/Greetings.java
@@ -2,5 +2,10 @@ package com.signavio.pair;
 
 public class Greetings {
 
+    public static String greet(String name)
+    {
+        return "Hello, " + name +".";
+    }
+
 
 }
--- src/test/java/com/signavio/pair/GreetingsTest.java
+++ src/test/java/com/signavio/pair/GreetingsTest.java
@@ -1,6 +1,19 @@
 package com.signavio.pair;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GreetingsTest {
+
+    @Test
+    public void testGreeting_greeting1_NameDisplayed()
+    {
+        String name = "Bob";
+
+        String result = Greetings.greet(name);
+
+        Assertions.assertEquals("Hello, Bob.", result);
+    }
 
 
 }
